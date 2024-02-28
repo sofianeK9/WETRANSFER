7b519608b8e6dd60d24859f340af5b4e8bd910bf
Sébastien <sebastien.maillet@gmail.com>
2024-02-28 15:33:47 +0100

    Ajout popups modifications profil

---
+++ pages/popup_modification_identifiant.php
@@ -0,0 +1,27 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $email = filter_input(INPUT_POST, "email");
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/popup_modification_identifiant.css">
+    <title>Modification du profil</title>
+</head>
+<body>
+    <form action="" class="form_modif_iden">
+        <legend class="legend">Modifier mon identifiant</legend>
+        <label for="email">Nouvel email : </label>
+        <input type="email" id="email" name="email" >
+
+        <input type="submit" value="Valider">
+    </form>
+</body>
+</html>
