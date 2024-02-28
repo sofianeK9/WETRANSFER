099197ca0623adacb514c71225e8ecae9c49d180
sebastien <sebastien.divers@gmail.com>
2024-02-28 18:15:54 +0100

    Simplification css popup modif profil

--- pages/popUpModificationIdentifiant.php
+++ pages/popUpModificationIdentifiant.php
@@ -12,7 +12,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/popup_modification_identifiant.css">
+    <link rel="stylesheet" href="../css/popUpModificationProfil.css">
     <title>Modification du profil</title>
 </head>
 <body>
--- pages/popUpModificationMdp.php
+++ pages/popUpModificationMdp.php
@@ -16,7 +16,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/popup_modification_mdp.css">
+    <link rel="stylesheet" href="../css/popUpModificationProfil.css">
     <title>Modification du profil</title>
 </head>
 <body>
