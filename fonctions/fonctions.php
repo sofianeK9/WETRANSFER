d6ca3d33937eb5c76486d8983551590458e307c6
sofcomlink <sofiane.kaya@comlink.fr>
2024-02-28 22:33:41 +0100

    css page connexion et inscription

--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -1,26 +1,7 @@
 <?php
-function inscription() {
+session_start();
+session_destroy();
+header("Location: /WeTransfer/pages/connexion.php");
+exit();
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $identifiant = filter_input(INPUT_POST, "identifiant");
-        $motDePasse = filter_input(INPUT_POST, "motDePasse");
-        $confirmationMotDePasse = filter_input(INPUT_POST, "confirmationMotDePasse");
-
-        if($identifiant != "" && $motDePasse === $confirmationMotDePasse) {
-
-            $fichier = fopen("informations.txt", "a");
-
-            fwrite($fichier, "$identifiant,$motDePasse,$confirmationMotDePasse\n");
-
-            fclose($fichier);
-
-            echo "inscription réussi !";
-
-            // header("Location, ")
-        }
-    } else {
-        echo "Veuillez remplir correctement tous les champs et vérifier que les mots de passe correspondent.";
-    }
-
-}
