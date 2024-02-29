33e4933af6442235b125ccf9abd7c22519eb0b23
jumariaco <justinemariacourt@hotmail.fr>
2024-02-29 18:59:40 +0100

    ajout affichage des 4 popup+affichage tableau mes fichiers

--- pages/fichiers.php
+++ pages/fichiers.php
@@ -1,8 +1,14 @@
 <?php 
-session_start();
-if (isset($_SESSION["connecte"])){
-    header:('Location: ../connexion.php');
-    exit();}
+//session_start();
+//if (!isset($_SESSION["connecte"])){
+    //header:('Location: ../pages/connexion.php');
+    //exit();}
+$fichiersUpload = '../fichiersUpload';
+$fichiers= scandir($fichiersUpload);
+$fichiers = array_diff($fichiers, array('.', '..'));
+//problème j'ai tous les fichiers
+//faire une variable avec QUE mes fichiers pr tableau 1 -> propriétaire = id de celui connecté
+//faire une variable avec fichiers dont je suis la cible pr tableau 2 -> cible = id de celui connecté
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,19 +21,21 @@ if (isset($_SESSION["connecte"])){
     <title>Liste des fichiers</title>
 </head>
 <header>
+    <!-- barre de navigation -->
 <nav class="navbar">
             <a href="#mesFichiers">Mes fichiers</a>
             <a href="#fichiersPartages">Fichiers partagés</a>
-            <a href="/WETRANSFER/pages/profil.php">Modifier mon profil</a>
-            <a href="/WeTransfer/fonctions/fonctions.php">Se déconnecter</a>
+            <a onclick="ouvrirModal('modalModifIdentifiant')">Modifier mon email</a>
+            <a onclick="ouvrirModal('modalModifMpd')">Modifier mon mot de passe</a>
+            <a href="../pages/deconnexion.php">Se déconnecter</a>
         </nav>
 </header>
 <body>
-    <a class="btn" onclick="ajoutFichier()" 
-    
-    >Ajouter un fichier</a>
+    <a class="btn" onclick="ouvrirModal('modalAjout')" >Ajouter un fichier</a>
+    <!-- tableau de mes fichiers -->
     <h2 id="mesFichiers">Mes fichiers</h2>
-    <?php //if (): ?>
+    <!-- si j'ai des fichiers, le tableau s'affiche -->
+    <?php if (!empty($fichiers)): ?>
     <table>
     <thead>
         <tr>
@@ -37,34 +45,27 @@ if (isset($_SESSION["connecte"])){
             <th>Détails</th>
         </tr>
     </thead>
-    <?php //foreach($fichiers as $fichier) ?>
     <tbody>
+        <!-- boucle sur chacun de mes fichiers -->
+    <?php foreach($fichiers as $f): ?>
         <tr>
-            <td>fef</td>
+            <td><?= $f ?></td>
+            <!-- ajout fonction JS pour delete : créer une popup de confirmation? -->
             <td><span class="material-symbols-outlined">delete</span></td>
+            <!-- ajout fonction JS pour download : créer une popup de confirmation? -->
             <td><span class="material-symbols-outlined">download</span></td>
-            <td><a class="btn" href="../pages/popUpDetails.php">Détails</a></td>
+            <td><a class="btn" onclick="ouvrirModal('modalDetails')">Détails</a></td>
         </tr>
-        <tr>
-            <td>zfz</td>
-            <td><span class="material-symbols-outlined">delete</span></td>
-            <td><span class="material-symbols-outlined">download</span></td>
-            <td><a class="btn" href="../pages/popUpDetails.php">Détails</a></td>
-        </tr>
-        <tr>
-            <td>fesg</td>
-            <td><span class="material-symbols-outlined">delete</span></td>
-            <td><span class="material-symbols-outlined">download</span></td>
-            <td><a class="btn" href="../pages/popUpDetails.php">Détails</a></td>
-        </tr>
+        <?php endforeach ?>
     </tbody>
     </table>
-    <?php //endforeach ?>
-    <?php //else: ?>
+    <!-- si je n'ai pas de fichier, pas de tableau, j'informe par un message -->
+    <?php else: ?>
     <p>Aucun fichier disponible</p>
-    <?php //endif ?>
-    
+    <?php endif ?>
+    <!-- tableau des fichiers qui me sont partagés -->
     <h2 id="fichiersPartages">Fichiers partagés</h2>
+    <!-- s'il y a des fichiers, le tableau s'affiche -->
     <?php //if (): ?>
     <table>
     <thead>
@@ -74,8 +75,9 @@ if (isset($_SESSION["connecte"])){
             <th>Télécharger</th>
         </tr>
     </thead>
-    <?php //foreach ?>
     <tbody>
+    <!-- boucle sur chacun des fichiers -->
+    <?php //foreach ?>
         <tr>
             <td>dggf</td>
             <td><span class="material-symbols-outlined">delete</span></td>
@@ -86,28 +88,51 @@ if (isset($_SESSION["connecte"])){
             <td><span class="material-symbols-outlined">delete</span></td>
             <td><span class="material-symbols-outlined">download</span></td>
         </tr>
-    </tbody>
     <?php //endforeach ?>
+    </tbody>
     </table>
+    <!-- s'il n'y a pas de fichier, pas de tableau, j'informe par un message -->
     <?php //else: ?>
     <p>Aucun fichier disponible</p>
     <?php //endif ?>
-    
+    <!-- ----------------------------------------------------------------
+                                    POP UPS 
+    --------------------------------------------------------------------->
+    <!--//Popup d'ajout de fichier-->
+    <div id="modalAjout" class="modal hidden">
+        <div class="modal-content">
+            <?php include '../pages/popUpAjoutFichier.php' ?>
+        </div>
+    </div>
+    <!--Popup de détails(ou mettre ds le meme modal avec des classe add remove ?)-->
+    <div id="modalDetails" class="modal hidden">
+        <div class="modal-content">
+            <?php include '../pages/popUpDetails.php' ?>
+        </div>
+    </div>
+    <!--//Popup de modif identifiant-->
+    <div id="modalModifIdentifiant" class="modal hidden">
+        <div class="modal-content">
+            <?php include '../pages/popUpModificationIdentifiant.php' ?>
+        </div>
+    </div>
+    <!--Popup de modif mdp-->
+    <div id="modalModifMdp" class="modal hidden">
+        <div class="modal-content">
+            <?php include '../pages/popUpModificationMdp.php' ?>
+        </div>
+    </div>
 </body>
 </html>
+    <!-- ----------------------------------------------------------------
+                             FONCTION JAVASCRIPT 
+    --------------------------------------------------------------------->
 <script>
-    function details(){
-        $.ajax({
-            type : 'POST',
-            url : '../pages/popUpDetails.php',
-            success:function(){
-                
-            }
-        })
-    }
-
-    function ajoutFichier() {
-        window.open('../pages/popUpAjoutFichier.php', '_blank', 'width=600,height=400,scrollbars=yes,resizable=yes');
-    }
-
+    //afficher le pop up sélectionné
+    function ouvrirModal(idModal) {
+    // Récupérer le modal
+    var modal = document.getElementById(idModal);
+    // afficher le modal
+    modal.classList.remove('hidden');
+}
 </script>
