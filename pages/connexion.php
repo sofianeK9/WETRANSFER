6bdbd13f8e429d636d6b0a6512947701e8b7c756
sofcomlink <sofiane.kaya@comlink.fr>
2024-02-29 15:59:45 +0100

    creation page connexion + inscription, renommage du fichier donnees.txt par identifiants.txt + creation des fonctions dans fonctions.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,33 +1,7 @@
-<?php
-
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $identifieur = filter_input(INPUT_POST, "identifieur");
-    $mdp = filter_input(INPUT_POST, 'mdp');
-
-    $personnes = [];
-    $fichier = file_get_contents('informations.txt');
-    $lignes = explode("\n", $fichier);
-    foreach ($lignes as $ligne) {
-        $identifiants = explode(",", $ligne);
-        $personnes[] = ["identifiant" => $identifiants[0], "motDePasse" => $identifiants[1], "confirmationmotDePasse" => $identifiants[2]];
-    }
-    header("Location: /WeTransfer/pages/fichiers.php");
-    exit();
-    if ( $identifieur != "" && $password != "" && $identifieur == $identifiant && $password == $mdp) {
-        $_SESSION["connecte"] = true;
-
-        header("Location: /WeTransfer/pages/connexion.php");
-        exit();
-    } elseif ($identifieur != $identifiant && $password != $mdp) {
-        echo "identifiant invalide !";
-    }
-}
-session_destroy();
-
+<?php require_once '../fonctions/fonctions.php';
+connexion();
+ini_set('display_errors', 1);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,8 +20,8 @@ session_destroy();
                 <label for="identifieur">Identifiant</label>
                 <input id="identifieur" name="identifieur" type="text">
 
-                <label id="mdp" name="mdp" for="mdp">Mot de passe</label>
-                <input id="mdp" name="mdp" type="text">
+                <label id="motDePasse" name="motDePasse" for="motDePasse">Mot de passe</label>
+                <input id="motDePasse" name="motDePasse" type="text">
 
                 <input id="submit" type="submit" value="connexion">
                 <a href="/WeTransfer/pages/inscription.php">Inscrivez-vous !</a>
---
+++ pages/deconnexion.php
@@ -0,0 +1,2 @@
+<?php require_once '../fonctions/fonctions.php';
+deconnexion();
