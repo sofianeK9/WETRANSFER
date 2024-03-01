e563f41b962d70bceed750673c5d7a447b4bd6d1
sebastien <sebastien.divers@gmail.com>
2024-03-01 14:08:01 +0100

    Modification popUpModificationIdentifiant et css associé

--- pages/popUpModificationIdentifiant.php
+++ pages/popUpModificationIdentifiant.php
@@ -1,6 +1,5 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupération des informations du formulaire ( ancien email, nouvel email )
     $ancienEmail = filter_input(INPUT_POST, "ancienEmail");
     $email = filter_input(INPUT_POST, "email");
@@ -20,27 +19,33 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/popUpModificationProfil.css">
+    <script src="../fonctions/fonctionsJavascript.js"></script>
     <title>Modification du profil</title>
 </head>
-<body>
-    <!-- Formulaire de modification du profil -->
-    <form method="POST" class="form_modif_iden">
-        <legend class="legend">Modifier mon identifiant</legend>
-        <label for="ancienEmail">Ancien email : </label>
-        <input type="email" id="ancienEmail" name="ancienEmail" >
-        <label for="email">Nouvel email : </label>
-        <input type="email" id="email" name="email" >
 
-        <input type="submit" value="Valider">
-    </form>
-    <!-- Affichage du message du ré -->
-    <?php if ($retourChangement); ?>
+<body>
+    <div class="container">
+        <!-- Formulaire de modification du profil -->
+        <form method="POST" class="form_modif_iden">
+            <legend class="legend">Modifier mon identifiant</legend>
+            <label for="ancienEmail">Ancien email : </label>
+            <input type="email" id="ancienEmail" name="ancienEmail">
+            <label for="email">Nouvel email : </label>
+            <input type="email" id="email" name="email">
+
+            <input type="submit" value="Valider">
+        </form>
+        <!-- Affichage du message du ré -->
+        <?php if ($retourChangement); ?>
         <p><?php echo $changement ?></p>
-    
+        <a onclick="fermerModal('modalModifIdentifiant')" class="btnClose">X</a>
+    </div>
 
 </body>
+
 </html>
