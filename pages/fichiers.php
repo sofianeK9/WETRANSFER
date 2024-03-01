ac89ed80628a5aa1b09bee7c2fc9d9fb4e68bc0f
jumariaco <justinemariacourt@hotmail.fr>
2024-03-01 15:35:19 +0100

    ajout variable identifiants et liste fichiers pour boucler dessus

--- pages/fichiers.php
+++ pages/fichiers.php
@@ -1,22 +1,36 @@
-<?php require_once '../fonctions/fonctions.php';
-//session_start();
-//if (!isset($_SESSION["connecte"])){
-    //header:('Location: ../pages/connexion.php');
-    //exit();}
-$fichiersUpload = '../fichiersUpload';
-$fichiers= scandir($fichiersUpload);
-$fichiers = array_diff($fichiers, array('.', '..'));
-//problème j'ai tous les fichiers
-//faire une variable avec QUE mes fichiers pr tableau 1 -> propriétaire = id de celui connecté
-//faire une variable avec fichiers dont je suis la cible pr tableau 2 -> cible = id de celui connecté
-
-
+<?php
+require_once '../fonctions/fonctions.php';
+session_start();
+if (!isset($_SESSION["connecte"])) {
+    header:
+    ('Location: ../pages/connexion.php');
+    exit();
+}
+//$fichiersUpload = '../fichiersUpload';
+//$fichiers = scandir($fichiersUpload);
+//$fichiers = array_diff($fichiers, array('.', '..'));
+//récupérer le tableau avec les données des fichiers
+$fichiersData = file_get_contents('../data/fichiersData.json');
+$fichiersDatas = json_decode($fichiersData, true);
+//créer des tableaux vides pour remplir le tableau 1 et 2
+$mesFichiers = [];
+$fichiersPartages = [];
+//remplir les tableaux précédents selon la liste des données de fichiers
+foreach ($fichiersDatas as $f) {
+    //si des fichiers ont pour propriétaire l'identifiant de la personne connectée
+    if ($f["emailPartage"] == $_SESSION["identifiant"]) {
+        $mesFichiers[] = $f;
+    //si des fichiers ont pour cible l'identifiant de la personne connectée
+    } elseif ($f["emailPartage"] == $_SESSION["identifiant"]) {
+        $fichiersPartages[] = $f;
+    }
+}
 $erreur = "";
-upload()
-
+upload();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,84 +42,84 @@ upload()
 </head>
 <header>
     <!-- barre de navigation -->
-<nav class="navbar">
-            <a href="#mesFichiers">Mes fichiers</a>
-            <a href="#fichiersPartages">Fichiers partagés</a>
-            <a onclick="ouvrirModal('modalModifIdentifiant')">Modifier mon email</a>
-            <a onclick="ouvrirModal('modalModifMpd')">Modifier mon mot de passe</a>
-            <a href="../pages/deconnexion.php">Se déconnecter</a>
-        </nav>
+    <nav class="navbar">
+        <a href="#mesFichiers">Mes fichiers</a>
+        <a href="#fichiersPartages">Fichiers partagés</a>
+        <a onclick="ouvrirModal('modalModifIdentifiant')">Modifier mon email</a>
+        <a onclick="ouvrirModal('modalModifMdp')">Modifier mon mot de passe</a>
+        <a href="../pages/deconnexion.php">Se déconnecter</a>
+    </nav>
 </header>
+
 <body>
+    <h2>Bonjour <?= $_SESSION["identifiant"] ?></h2>
     <div id="contain">
-        <a class="btn btnAjout" onclick="ouvrirModal('modalAjout')" >Ajouter un fichier</a>
+        <a class="btn btnAjout" onclick="ouvrirModal('modalAjout')">Ajouter un fichier</a>
         <!-- tableau de mes fichiers -->
         <h2 id="mesFichiers">Mes fichiers</h2>
         <!-- si j'ai des fichiers, le tableau s'affiche -->
-        <?php if (!empty($fichiers)): ?>
-        <table>
-        <thead>
-            <tr>
-                <th>Nom du fichier</th>
-                <th>Supprimer</th>
-                <th>Retélécharger</th>
-                <th>Détails</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- boucle sur chacun de mes fichiers -->
-        <?php foreach($fichiers as $f): ?>
-            <tr>
-                <td><?= $f ?></td>
-                <!-- ajout fonction JS pour delete : créer une popup de confirmation? -->
-                <td><span onclick="ouvrirModal('modalDelete')" class="material-symbols-outlined">delete</span></td>
-                <!-- ajout fonction JS pour download : créer une popup de confirmation? -->
-                <td><span class="material-symbols-outlined">download</span></td>
-                <td><a class="btn" onclick="ouvrirModal('modalDetails')">Détails</a></td>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-        </table>
-        <!-- si je n'ai pas de fichier, pas de tableau, j'informe par un message -->
-        <?php else: ?>
-        <p>Aucun fichier disponible</p>
+        <?php if (!empty($mesFichiers)) : ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom du fichier</th>
+                        <th>Supprimer</th>
+                        <th>Retélécharger</th>
+                        <th>Détails</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- boucle sur chacun de mes fichiers -->
+                    <?php foreach ($fichiers as $f) : ?>
+                        <tr>
+                            <td><?= $f ?></td>
+                            <!-- ajout fonction JS pour delete au niveau de la popup de confirmation? -->
+                            <td><span onclick="ouvrirModal('modalDelete')" class="material-symbols-outlined">delete</span></td>
+                            <!-- ajout fonction JS pour download -->
+                            <td><a <?php //readfile($f) ou file_get_contents($f) 
+                                    ?> href="../fichiersUpload/<?= $f ?>" download><span class="material-symbols-outlined">download</span></td>
+                            <td><a class="btn" onclick="ouvrirModal('modalDetails')">Détails</a></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+            <!-- si je n'ai pas de fichier, pas de tableau, j'informe par un message -->
+        <?php else : ?>
+            <p>Aucun fichier disponible</p>
         <?php endif ?>
         <!-- tableau des fichiers qui me sont partagés -->
         <h2 id="fichiersPartages">Fichiers partagés</h2>
         <!-- s'il y a des fichiers, le tableau s'affiche -->
-        <?php //if (): ?>
+        <?php if (!empty($fichiersPartages)) : ?>
         <table>
-        <thead>
-            <tr>
-                <th>Nom du fichier</th>
-                <th>Supprimer</th>
-                <th>Télécharger</th>
-            </tr>
-        </thead>
-        <tbody>
-        <!-- boucle sur chacun des fichiers -->
-        <?php //foreach ?>
-            <tr>
-                <td>dggf</td>
-                <td><span class="material-symbols-outlined">delete</span></td>
-                <td><span class="material-symbols-outlined">download</span></td>
-            </tr>
-            <tr>
-                <td>dggf</td>
-                <td><span class="material-symbols-outlined">delete</span></td>
-                <td><span class="material-symbols-outlined">download</span></td>
-            </tr>
-        <?php //endforeach ?>
-        </tbody>
+            <thead>
+                <tr>
+                    <th>Nom du fichier</th>
+                    <th>Supprimer</th>
+                    <th>Télécharger</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- boucle sur chacun des fichiers -->
+                <?php foreach ($fichiersPartages as $f) : ?>
+                    <tr>
+                        <td><?= $f ?></td>
+                        <!-- ajout fonction JS pour delete au niveau de la popup de confirmation -->
+                        <td><span onclick="ouvrirModal('modalDelete')" class="material-symbols-outlined">delete</span></td>
+                        <!-- ajout fonction JS pour download -->
+                        <td><span class="material-symbols-outlined">download</span></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
         </table>
         <!-- s'il n'y a pas de fichier, pas de tableau, j'informe par un message -->
-        <?php //else: ?>
+        <?php else :?>
         <p>Aucun fichier disponible</p>
-        <?php //endif ?>
+        <?php endif ?>
     </div>
     <!-- ----------------------------------------------------------------
-                                    POP UPS 
-    --------------------------------------------------------------------->
+POP UPS 
+--------------------------------------------------------------------->
     <!--//Popup d'ajout de fichier-->
     <div id="modalAjout" class="modal hidden">
         <div class="modal-content">
@@ -138,7 +152,6 @@ upload()
             <a onclick="fermerModal('modalDelete')" class="btnClose">X</a>
         </div>
     </div>
-
 </body>
-</html>
 
+</html>
