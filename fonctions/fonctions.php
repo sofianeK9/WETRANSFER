81acd7713b555a01a68c25e1d321d45b9418f3eb
sofcomlink <sofiane.kaya@comlink.fr>
2024-03-02 17:57:42 +0100

    "validation"

--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -84,7 +84,7 @@ function upload()
 
         
         $emailPartager = filter_input(INPUT_POST, "emailPartage", FILTER_VALIDATE_EMAIL);
-        $dataFichiers = fopen('../data/dataFichiers.json', "a");
+        $dataFichiers = fopen('../data/dataFichiers.json', "w");
         fwrite($dataFichiers, "$emailPartager\n");
         fclose($dataFichiers);
         $nombreTelechargement = 0;
